74360cbaa1d1b1a1f9e4faf2f5649799adb34f04
manishsaraan <manish021js@gmail.com>
2019-03-04 18:07:38 +0530

    complete set methods with es6 set

--- sets.js
+++ sets.js
@@ -49,4 +49,59 @@ class MySets {
     secondSet.forEach(e => unionSet.add(e));
     return unionSet;
   }
+
+  // this method will return the intersection of two sets
+  intersection(otherSet) {
+    var intersectionSet = new MySets();
+    var firstSet = this.values();
+    firstSet.forEach(e => {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+    });
+
+    return intersectionSet;
+  }
+
+  // this method will return the difference of two sets
+  difference(otherSet) {
+    var differenceSet = new MySets();
+    var firstSet = this.values();
+    firstSet.forEach(e => {
+      if (!otherSet.has(e)) {
+        differenceSet.push(e);
+      }
+    });
+
+    return differenceSet;
+  }
+
+  // this method will test if the set is subset of a different set
+  subset(otherSet) {
+    var firstSet = this.values();
+    return firstSet.every(value => otherSet.has(value));
+  }
 }
+
+var setA = new MySets();
+var setB = new MySets();
+setA.add("a");
+setB.add("b");
+setB.add("c");
+setB.add("a");
+setB.add("d");
+console.log(setA.subset(setB));
+console.log(setA.intersection(setB).values());
+
+//es6 set
+var setC = new Set();
+var setD = new Set();
+setC.add("a");
+setD.add("b");
+setD.add("c");
+setD.add("a");
+setD.add("d");
+console.log(setD.values());
+setD.delete("a");
+console.log(setD.has("a"));
+console.log(setD.add("a"));
