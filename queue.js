9cb2512eea90c736d1d534e2da68b8cb3613c260
manishsaraan <manish021js@gmail.com>
2019-03-06 10:26:30 +0530

    creating priority queues

--- queue.js
+++ queue.js
@@ -39,3 +39,55 @@ queue.dequeue();
 console.log(queue.front());
 console.log(queue.size());
 queue.print();
+
+// priority queue
+
+class PriorityQueue {
+  constructor() {
+    this.collection = [];
+  }
+  print() {
+    console.log(this.collection);
+  }
+
+  enqueue(element) {
+    if (this.isEmpty()) {
+      this.collection.push(element);
+    } else {
+      let added = false;
+      for (let i = 0; i < this.collection.length; i++) {
+        if (element[1] < this.collection[i][1]) {
+          // checking the priority
+          this.collection.splice(i, 0, element);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        this.collection.push(element);
+      }
+    }
+  }
+
+  isEmpty() {
+    return this.collection.length === 0;
+  }
+
+  dequeue() {
+    return this.collection.shift()[0];
+  }
+
+  front() {
+    return this.collection[0];
+  }
+
+  size() {
+    return this.collection.length;
+  }
+}
+
+var pq = new PriorityQueue();
+pq.enqueue(["Beaue", 2]);
+pq.enqueue(["Beaue 1", 1]);
+pq.enqueue(["Beaue 3", 3]);
+pq.print();
