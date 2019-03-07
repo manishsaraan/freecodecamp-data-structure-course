5fdc9f5decb547b1344f37736a487e4b595ecd78
manishsaraan <manish021js@gmail.com>
2019-03-07 11:13:16 +0530

    binary tree structure

---
+++ binary-tree.js
@@ -0,0 +1,136 @@
+// Binary search tree
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  add(data) {
+    const node = this.root;
+    // if there is no root node
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      // search tree and insert node
+      const searchTree = function(node) {
+        // if data is less than node data, insert on left side
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+          // if data is less than node data, insert on right side
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      return searchTree(node);
+    }
+  }
+
+  findMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  findMax() {
+    let current = this.root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
+
+  isPresent(data) {
+    let current = this.root;
+    while (current) {
+      if (data === current.data) {
+        return true;
+      }
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+
+  remove(data) {
+    const removeNode = function(node, data) {
+      // if tree is empty
+      if (node === null) {
+        return null;
+      }
+      // if data is matched with node
+      if (data === node.data) {
+        // node has no children
+        if (node.left === null && node.right === null) {
+          return null;
+        }
+
+        // node has left child
+        if (node.left === null) {
+          return node.right;
+        }
+
+        // node has right child
+        if (node.right === null) {
+          return node.left;
+        }
+
+        // node has two children then get right side element of node and search left most in the right side subtree of node
+        var tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+
+    this.root = removeNode(this.root, data);
+  }
+}
+
+const bst = new BST();
+bst.add(4);
+bst.add(2);
+bst.add(6);
+bst.add(1);
+bst.add(3);
+bst.add(5);
+bst.add(7);
+bst.remove(4);
+console.log(bst.findMax());
+console.log(bst.findMin());
+bst.remove(7);
+console.log(bst.findMax());
+console.log(bst.isPresent(4));
